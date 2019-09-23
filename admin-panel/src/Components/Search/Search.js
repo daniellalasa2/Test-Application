e8549c3ca9526ed930710679a852a57d72a8343b
daniel <daniel.lalasa@gmail.com>
2019-09-23 19:53:01 +0330

    search spinner and loading behavior implemented

--- admin-panel/src/Components/Search/Search.js
+++ admin-panel/src/Components/Search/Search.js
@@ -3,12 +3,96 @@ import "./Search.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faSortDown } from "@fortawesome/free-solid-svg-icons";
 import { Table } from "reactstrap";
+import { GetSearchResult, SafeValue } from "../ApiHandler/ApiHandler";
+import Spinner from "../Tools/Spinner/Spinner";
+import classnames from "classnames";
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      totalEntries: 0,
+      sort: "asc",
+      doesUserSearching: false,
+      pagination: {
+        curr: "",
+        total: ""
+      }
+    };
   }
-  doSearch = () => {};
+  doSearch = e => {
+    var value = e.target.value;
+    this.setState({ doesUserSearching: true });
+    GetSearchResult({ search: value }, item => {
+      console.log(item);
+      if (item.success_result.success) {
+        this.setState(
+          {
+            doesUserSearching: false,
+            totalEntries: SafeValue(
+              item,
+              "data.meta.total_entries",
+              "number",
+              0
+            )
+          },
+          () => console.log(this.state)
+        );
+      }
+    });
+  };
+  doSort = () => {
+    GetSearchResult({}, item => {
+      if (item.success_result.success) {
+        console.log(item);
+        this.setState(
+          {
+            totalEntries: SafeValue(
+              item,
+              "data.meta.total_entries",
+              "number",
+              0
+            )
+          },
+          () => console.log(this.state)
+        );
+      }
+    });
+  };
+  doPaginate = type => {
+    let pagination_type = false;
+    switch (type) {
+      case "next":
+        break;
+      case "prev":
+        break;
+        break;
+      default:
+    }
+    if (!type) {
+      GetSearchResult({}, item => {
+        console.log(item);
+        if (item.success_result.success) {
+          this.setState(
+            {
+              totalEntries: SafeValue(
+                item,
+                "data.meta.total_entries",
+                "number",
+                0
+              )
+            },
+            () => console.log(this.state)
+          );
+        }
+      });
+    }
+  };
+  componentDidMount() {
+    // this.doSearch();
+  }
+
   render() {
+    const { totalEntries, doesUserSearching } = this.state;
     return (
       <div className="Search">
         {/* Search Section Elements */}
@@ -25,9 +109,20 @@ export default class Search extends React.Component {
                 color="whitesmoke"
               />
             </span>
-            <input type="text" className="searchField" />
-            <span className="searchButton-wrapper">
-              <button onClick={null}>SUCHEN</button>
+            <input
+              type="text"
+              className="searchField"
+              onChange={this.doSearch}
+            />
+            <span
+              className={classnames(
+                "searchButton-wrapper",
+                !doesUserSearching && "active"
+              )}
+            >
+              <button disabled={doesUserSearching} onClick={this.doSearch}>
+                {doesUserSearching ? <Spinner /> : "SUCHEN"}
+              </button>
             </span>
           </div>
           <div className="addNewRecord-box">
@@ -39,7 +134,9 @@ export default class Search extends React.Component {
         <div className="searchBody-section">
           <div className="menu-section">menu section</div>
           <div className="dataInfoTable-section">
-            <span className="resultCount">9 Ergebnisse gefunden</span>
+            <span className="resultCount">
+              {totalEntries} Ergebnisse gefunden
+            </span>
             <Table>
               <thead>
                 <tr>
@@ -48,7 +145,7 @@ export default class Search extends React.Component {
                     Name{" "}
                     <FontAwesomeIcon
                       icon={faSortDown}
-                      size="md"
+                      size="1x"
                       color="whitesmoke"
                     />
                   </th>
